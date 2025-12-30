aa4519ac251516053463c594f8720b12649d2ddb
Sai <sarany1947@gmail.com>
2025-12-30 14:51:54 -0600

    Day 1: Implementation of Identity Foundation

--- modules/iam/main.tf
+++ modules/iam/main.tf
@@ -13,6 +13,8 @@ resource "google_iam_workload_identity_pool_provider" "github" {
     "attribute.repository" = "assertion.repository"
   }
 
+  attribute_condition = "assertion.repository == '${var.github_repo}'"
+
   oidc { issuer_uri = "https://token.actions.githubusercontent.com" }
 }
 
--- modules/iam/variables.tf
+++ modules/iam/variables.tf
@@ -2,3 +2,8 @@ variable "project_id" {
   type        = string
   description = "The GCP Project ID"
 }
+
+variable "github_repo" {
+  description = "The Github reposithiroy in repo fromajt"
+  type        = string
+}
