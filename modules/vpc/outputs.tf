98742052334d8ffee553d00384e2b6577fedc905
Sai <sarany1947@gmail.com>
2026-01-05 13:04:57 -0600

    Day 1: Implementation of Identity Foundation

--- modules/vpc/outputs.tf
+++ modules/vpc/outputs.tf
@@ -1,4 +1,4 @@
 output "network_name" { value = google_compute_network.vpc.name }
-output "subnet_name" { value = google_compute_subnetwork.subnet.name }
+output "subnet_name" { value = google_compute_subnetwork.privateSubnet.name }
 output "pods_range_name" { value = "pods" }
 output "services_range_name" { value = "services" }
